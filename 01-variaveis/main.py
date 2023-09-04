8db36b7d947d011d6503f5bfaeddacb96569ebd8
Carlos Robert <crobertdmelo@gmail.com>
2023-09-04 01:05:06 -0300

    Initial commit

---
+++ 01-variaveis/main.py
@@ -0,0 +1,27 @@
+# # VARIÁVEIS
+
+# # strings
+# nome = 'Robert'
+# print("Olá " + nome + "!")
+# print(type(nome))
+# primeiro_nome = 'Carlos'
+# segundo_nome = 'Robert'
+# nome_completo = primeiro_nome + " " + segundo_nome
+# print(nome_completo)
+
+# números inteiros
+# idade = 28
+# idade = idade + 1
+# print(idade)
+# idade += 1
+# print(idade)
+# print(type(idade))
+# # print("Sua idade é: " + idade) # Erro
+# print("Sua idade é: ", idade)
+# print("Sua idade é: " + str(idade))
+# print(f"Sua idade é: {idade}")
+
+# números decimais
+
+
+
